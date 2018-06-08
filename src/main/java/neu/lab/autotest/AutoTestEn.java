0b3858ded0f13659a71f2f630ece07188a817ef6
DeccaDc <lzwneu@163.com>
2018-06-08 09:51:03 +0800

    1

--- src/main/java/neu/lab/autotest/AutoTestEn.java
+++ src/main/java/neu/lab/autotest/AutoTestEn.java
@@ -2,8 +2,10 @@ package neu.lab.autotest;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import neu.lab.autoexe.util.FileSyn;
 import neu.lab.autoexe.util.PomFinder;
@@ -16,9 +18,14 @@ public class AutoTestEn {
 	// static int exedNum = 0;
 	static String cls_type = "cls";
 	static String pomRoot = "D:\\ws\\gitHub_old";
-	static String distanceRoot = "D:\\ws_testcase\\distance_cls";
+	static String distanceRoot = "D:\\ws_testcase\\distance_cls\\";
 	static String stateDir = "D:\\ws_testcase\\image\\state_autoEvo\\";
 	static FileSyn doneProject;// record project-mvn-coordinate
+	static Set<String> skipProjects;
+	{
+		skipProjects = new HashSet<String>();
+		skipProjects.add(distanceRoot+"level_3_eagle+eagle-embed-hbase+0.3.0-incubating.txt");
+	}
 
 	public static void main(String[] args) throws Exception {
 		File stateDirFile = new File(stateDir);
@@ -29,6 +36,9 @@ public class AutoTestEn {
 		Map<String, String> id2path = new PomFinder().getId2path(new File(pomRoot));
 		List<File> highLevelFiles = getHighLevelFiles(new File(distanceRoot));
 		for (File highLevelFile : highLevelFiles) {
+			if(skipProjects.contains(highLevelFile.getAbsolutePath())) {
+				continue;
+			}
 			String mvnId = name2id(highLevelFile.getName());
 			if (!doneProject.contains(mvnId)) {// have not done
 				String pomPath = id2path.get(mvnId);
@@ -49,7 +59,6 @@ public class AutoTestEn {
 		// "D:\\ws_testcase\\distance_cls\\level_3_org.apache.accumulo+accumulo-master+1.7.2.txt";
 		// runOneProject(pomPath, distanceFile, cls_type);
 	}
-
 	private static List<File> getHighLevelFiles(File distanceRoot) {
 		List<File> highLevelFiles = new ArrayList<File>();
 		for (File distanceFile : distanceRoot.listFiles()) {
